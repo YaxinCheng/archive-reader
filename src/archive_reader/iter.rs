0ac98c2c0b2d1898c2a12dc82044f1269acf391a
Yaxin Cheng <Yaxin.Cheng@icloud.com>
2023-01-15 15:02:48 -0500

    Read content functions (#1)
    * Add functions in `ArchiveReader` to read file content
    * `BlockReader` added to read a file one block at a time
    * `EntryIterBorrowed` is added to iterate through entries
    * `EntryIter` is now a wrapper for `EntryIterBorrowed`, which contains the logic of iterating through entries

--- src/archive_reader/iter.rs
+++ src/archive_reader/iter.rs
@@ -2,10 +2,12 @@ use crate::error::{analyze_result, Error, Result};
 use crate::{libarchive, ArchiveReader};
 use log::{debug, error};
 use std::ffi::CStr;
+use std::slice;
 
-pub struct EntryIter<F> {
-    reader: ArchiveReader,
-    decoding: F,
+pub(crate) struct EntryIter<F> {
+    /// _reader_guard prevents the ArchiveReader from drop until the Iterator itself is dropped.
+    _reader_guard: ArchiveReader,
+    iterator: EntryIterBorrowed<F>,
 }
 
 impl<F> EntryIter<F>
@@ -13,7 +15,11 @@ where
     F: Fn(&[u8]) -> Option<String>,
 {
     pub fn new(reader: ArchiveReader, decoding: F) -> Self {
-        Self { reader, decoding }
+        let iterator = EntryIterBorrowed::new(reader.handle, decoding);
+        Self {
+            _reader_guard: reader,
+            iterator,
+        }
     }
 }
 
@@ -24,15 +30,40 @@ where
     type Item = Result<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.iterator.next()
+    }
+}
+
+pub(crate) struct EntryIterBorrowed<F> {
+    handle: *mut libarchive::archive,
+    decoding: F,
+}
+
+impl<F> EntryIterBorrowed<F>
+where
+    F: Fn(&[u8]) -> Option<String>,
+{
+    pub fn new(handle: *mut libarchive::archive, decoding: F) -> Self {
+        Self { handle, decoding }
+    }
+}
+
+impl<F> Iterator for EntryIterBorrowed<F>
+where
+    F: Fn(&[u8]) -> Option<String>,
+{
+    type Item = Result<String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         let mut entry = std::ptr::null_mut();
         unsafe {
-            match libarchive::archive_read_next_header(self.reader.handle, &mut entry) {
+            match libarchive::archive_read_next_header(self.handle, &mut entry) {
                 1 /*libarchive::ARCHIVE_EOF*/ => {
                     debug!("archive_read_next_header: reaches EOF");
                     return None
                 },
                 result => {
-                    if let Err(error) = analyze_result(result, self.reader.handle) {
+                    if let Err(error) = analyze_result(result, self.handle) {
                         error!("archive_read_next_header error: {error}");
                         return Some(Err(error))
                     }
@@ -55,6 +86,41 @@ where
                     Some(Err(Error::Encoding))
                 }
             }
+        }
+    }
+}
+
+pub struct BlockReader(ArchiveReader);
+
+impl BlockReader {
+    pub fn new(archive_reader: ArchiveReader) -> Self {
+        BlockReader(archive_reader)
+    }
+}
+
+impl Iterator for BlockReader {
+    type Item = Result<Box<[u8]>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buf = std::ptr::null();
+        let mut offset = 0;
+        let mut size = 0;
+
+        unsafe {
+            match libarchive::archive_read_data_block(self.0.handle, &mut buf, &mut size, &mut offset) {
+                1 /*libarchive::ARCHIVE_EOF*/ => {
+                    debug!("archive_read_data_block: reaches eof");
+                    return None
+                },
+                result => {
+                    if let Err(error) = analyze_result(result, self.0.handle) {
+                        error!("archive_read_data_block error: {error}");
+                        return Some(Err(error))
+                    }
+                }
+            };
+            let content = slice::from_raw_parts(buf as *const u8, size);
+            Some(Ok(content.into()))
         }
     }
 }
--- src/archive_reader/reader.rs
+++ src/archive_reader/reader.rs
@@ -2,9 +2,12 @@ use super::iter;
 use crate::error::{analyze_result, path_does_not_exist, Error, Result};
 use log::{error, info};
 use std::ffi::CString;
+use std::io::Write;
 use std::path::Path;
 
 use crate::libarchive;
+
+pub type Bytes = Box<[u8]>;
 
 pub struct ArchiveReader {
     pub(crate) handle: *mut libarchive::archive,
@@ -54,8 +57,93 @@ impl ArchiveReader {
     where
         F: Fn(&[u8]) -> Option<String>,
     {
-        info!("ArchiveReader::list_file_names_with_encoding()");
+        info!("ArchiveReader::list_file_names_with_encoding(decoding: _)");
         iter::EntryIter::new(self, decoding)
+    }
+
+    pub fn read_file_to_bytes(self, file_name: &str) -> Result<Vec<u8>> {
+        info!(r#"ArchiveReader::read_file_to_bytes("file_name: {file_name}")"#);
+        let mut combined = Vec::new();
+        self.read_file(file_name, &mut combined)?;
+        Ok(combined)
+    }
+
+    pub fn read_file(self, file_name: &str, mut output: impl Write) -> Result<usize> {
+        info!(r#"ArchiveReader::read_file("file_name: {file_name}", output: _)"#);
+        let mut total_read = 0;
+        for bytes in self.read_file_by_block(file_name)? {
+            let bytes = bytes?;
+            total_read += bytes.len();
+            output.write_all(bytes.as_ref())?;
+        }
+        Ok(total_read)
+    }
+
+    pub fn read_file_to_bytes_with_encoding<F>(
+        self,
+        file_name: &str,
+        decoding: F,
+    ) -> Result<Vec<u8>>
+    where
+        F: Fn(&[u8]) -> Option<String>,
+    {
+        info!(
+            r#"ArchiveReader::read_file_to_bytes_with_encoding("file_name: {file_name}", decoding: _)"#
+        );
+        let mut combined = Vec::new();
+        self.read_file_with_encoding(file_name, &mut combined, decoding)?;
+        Ok(combined)
+    }
+
+    pub fn read_file_with_encoding<W, F>(
+        self,
+        file_name: &str,
+        mut output: W,
+        decoding: F,
+    ) -> Result<usize>
+    where
+        W: Write,
+        F: Fn(&[u8]) -> Option<String>,
+    {
+        info!(
+            r#"ArchiveReader::read_file_with_encoding("file_name: {file_name}", output: _, decoding: _)"#
+        );
+        let mut total_read = 0;
+        for bytes in self.read_file_by_block_with_encoding(file_name, decoding)? {
+            let bytes = bytes?;
+            total_read += bytes.len();
+            output.write_all(bytes.as_ref())?;
+        }
+        Ok(total_read)
+    }
+
+    pub fn read_file_by_block(
+        self,
+        file_name: &str,
+    ) -> Result<impl Iterator<Item = Result<Bytes>>> {
+        info!(r#"ArchiveReader::read_file_by_block("file_name: {file_name}")"#);
+        self.read_file_by_block_with_encoding(file_name, |entry_name| {
+            Some(String::from_utf8_lossy(entry_name).to_string())
+        })
+    }
+
+    pub fn read_file_by_block_with_encoding<F>(
+        self,
+        file_name: &str,
+        decoding: F,
+    ) -> Result<impl Iterator<Item = Result<Bytes>>>
+    where
+        F: Fn(&[u8]) -> Option<String>,
+    {
+        info!(
+            r#"ArchiveReader::read_file_by_blockwith_encoding(file_name: "{file_name}", decoding: _)"#
+        );
+        for entry_name in iter::EntryIterBorrowed::new(self.handle, decoding) {
+            if entry_name? == file_name {
+                break;
+            }
+        }
+        Ok(iter::BlockReader::new(self))
     }
 
     fn clean(&self) -> Result<()> {
