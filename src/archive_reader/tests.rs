b673b865c6962fb1e44953137de3738a8da624d9
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2023-01-21 13:25:15 -0500

    Add tests for not existing file in archive and empty file

--- src/archive_reader/tests.rs
+++ src/archive_reader/tests.rs
@@ -72,6 +72,18 @@ fn test_read_rar() -> Result<()> {
     test_read_file_to_bytes(rar_archive(), "content/nested/second", b"second\n")
 }
 
+#[test]
+#[should_panic]
+fn test_read_non_existing_file() {
+    test_read_file_to_bytes(zip_archive(), "not_existed", b"").unwrap()
+}
+
+#[test]
+fn test_empty_file() -> Result<()> {
+    let zip_path = concat!(env!("CARGO_MANIFEST_DIR"), "/test_resources/empty.zip");
+    test_read_file_to_bytes(zip_path, "empty", b"")
+}
+
 fn test_read_file_to_bytes(archive_path: &str, file_path: &str, expected: &[u8]) -> Result<()> {
     let archive = ArchiveReader::open(archive_path)?;
     let bytes = archive.read_file_to_bytes(file_path)?;
@@ -98,9 +110,31 @@ fn test_read_by_blocks() -> Result<()> {
     while let Some(block) = blocks.next() {
         let block = block?;
         num_of_blocks += 1;
-        bytes.extend(block.into_iter());
+        bytes.extend(block.iter());
     }
     assert!(num_of_blocks > 1);
     assert_eq!(expected, bytes.as_slice());
+    Ok(())
+}
+
+#[test]
+fn test_empty_by_block() -> Result<()> {
+    #[cfg(feature = "lending_iter")]
+    use crate::LendingIterator;
+
+    let archive = ArchiveReader::open(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/test_resources/empty.zip"
+    ))?;
+    let mut blocks = archive.read_file_by_block("empty")?;
+    let mut number_of_blocks = 0;
+    let mut bytes = Vec::<u8>::new();
+    while let Some(block) = blocks.next() {
+        let block = block?;
+        number_of_blocks += 1;
+        bytes.extend(block.iter());
+    }
+    assert_eq!(number_of_blocks, 0);
+    assert_eq!(bytes, &[]);
     Ok(())
 }
