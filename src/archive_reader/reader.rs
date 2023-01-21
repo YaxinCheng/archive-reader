97a495f490cba82c03593b9edc0ea1845a821f4e
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2023-01-21 14:00:14 -0500

    Add documentation to the crate

--- src/archive_reader/reader.rs
+++ src/archive_reader/reader.rs
@@ -9,29 +9,49 @@ use crate::libarchive;
 #[cfg(feature = "lending_iter")]
 use crate::LendingIterator;
 
+/// `ArchiveReader` is a type that handles the archive reading task.
+/// It wraps partial functionalities of libarchive to read archives.
+///
+/// # Note:
+/// As libarchive does not support random access,
+/// every function in ArchiveReader consumes itself.
 pub struct ArchiveReader {
     pub(crate) handle: *mut libarchive::archive,
 }
 
+// pointer to an libarchive struct is safe to move.
 unsafe impl Send for ArchiveReader {}
 
+/// BLOCK_SIZE is the size for each block to be read in through ArchiveReader.
 const BLOCK_SIZE: usize = 16 * 1024;
 
 impl ArchiveReader {
+    /// `open` is the constructor for ArchiveReader.
+    /// It takes in the path to the archive.
     pub fn open<P: AsRef<Path>>(archive_path: P) -> Result<Self> {
         let archive_path = archive_path.as_ref();
         info!(
             r#"ArchiveReader::open(archive_path: "{}")"#,
             archive_path.display()
         );
+        Self::path_exists(archive_path)?;
+        let handle = Self::create_handle(archive_path)?;
+        Ok(ArchiveReader { handle })
+    }
+
+    fn path_exists(archive_path: &Path) -> Result<()> {
         if !archive_path.exists() {
             error!(r#"path "{}" does not exist"#, archive_path.display());
             return Err(path_does_not_exist(
                 archive_path.to_string_lossy().to_string(),
             ));
         }
-        let archive_path =
-            CString::new(archive_path.to_str().ok_or(Error::PathNotUtf8)?).expect("Not null");
+        Ok(())
+    }
+
+    fn create_handle(archive_path: &Path) -> Result<*mut libarchive::archive> {
+        let archive_path = CString::new(archive_path.to_str().ok_or(Error::PathNotUtf8)?)
+            .expect("An existing path cannot be null");
         unsafe {
             let handle = libarchive::archive_read_new();
             analyze_result(libarchive::archive_read_support_filter_all(handle), handle)?;
@@ -41,15 +61,18 @@ impl ArchiveReader {
                 libarchive::archive_read_open_filename(handle, archive_path.as_ptr(), BLOCK_SIZE),
                 handle,
             )?;
-            Ok(ArchiveReader { handle })
+            Ok(handle)
         }
     }
 
+    /// `list_file_names` extracts file names from the target archive using UTF8 encoding.
     pub fn list_file_names(self) -> impl Iterator<Item = Result<String>> {
         info!("ArchiveReader::list_file_names()");
         self.list_file_names_with_encoding(|bytes| Some(String::from_utf8_lossy(bytes).to_string()))
     }
 
+    /// `list_file_names_with_encoding` extracts file names from the target archive
+    /// using custom decoding function.
     pub fn list_file_names_with_encoding<F>(
         self,
         decoding: F,
@@ -61,6 +84,8 @@ impl ArchiveReader {
         iter::EntryIter::new(self, decoding)
     }
 
+    /// `read_file_to_bytes` locates a file based on its file name in UTF8 and reads its content back
+    /// as a Vec of bytes.
     pub fn read_file_to_bytes(self, file_name: &str) -> Result<Vec<u8>> {
         info!(r#"ArchiveReader::read_file_to_bytes("file_name: {file_name}")"#);
         let mut combined = Vec::new();
@@ -68,6 +93,9 @@ impl ArchiveReader {
         Ok(combined)
     }
 
+    /// `read_file` locates a file based on its file name in UTF8 and reads its content
+    /// into a provided writable output.
+    /// Eventually, it returns the size for total bytes read.
     pub fn read_file(self, file_name: &str, mut output: impl Write) -> Result<usize> {
         info!(r#"ArchiveReader::read_file("file_name: {file_name}", output: _)"#);
         let mut total_read = 0;
@@ -80,6 +108,9 @@ impl ArchiveReader {
         Ok(total_read)
     }
 
+    /// `read_file_to_bytes_with_encoding` locates a file based on its file name
+    /// in provided encoding and reads its content
+    /// into vec byte.
     pub fn read_file_to_bytes_with_encoding<F>(
         self,
         file_name: &str,
@@ -96,6 +127,10 @@ impl ArchiveReader {
         Ok(combined)
     }
 
+    /// `read_file_with_encoding` locates a file based on its file name
+    /// in provided encoding and reads its content
+    /// into a provided writable output.
+    /// Eventually, it returns the size for total bytes read.
     pub fn read_file_with_encoding<W, F>(
         self,
         file_name: &str,
@@ -119,6 +154,8 @@ impl ArchiveReader {
         Ok(total_read)
     }
 
+    /// `read_file_by_block` locates a file based on its UTF8 encoded file name,
+    /// and reads its content as an iterator of blocks.
     #[cfg(not(feature = "lending_iter"))]
     pub fn read_file_by_block(
         self,
@@ -130,6 +167,8 @@ impl ArchiveReader {
         })
     }
 
+    /// `read_file_by_block` locates a file based on its UTF8 encoded file name,
+    /// and reads its content as a lending iterator of blocks.
     #[cfg(feature = "lending_iter")]
     pub fn read_file_by_block(
         self,
@@ -141,6 +180,9 @@ impl ArchiveReader {
         })
     }
 
+    /// `read_file_by_block_with_encoding` locates a file based on its file name
+    /// with custom decoding function,
+    /// and reads its content as a lending iterator of blocks.
     #[cfg(feature = "lending_iter")]
     pub fn read_file_by_block_with_encoding<F>(
         self,
@@ -153,6 +195,9 @@ impl ArchiveReader {
         self.read_file_by_block_with_encoding_raw(file_name, decoding)
     }
 
+    /// `read_file_by_block_with_encoding` locates a file based on its file name
+    /// with custom decoding function,
+    /// and reads its content as an iterator of blocks.
     #[cfg(not(feature = "lending_iter"))]
     pub fn read_file_by_block_with_encoding<F>(
         self,
